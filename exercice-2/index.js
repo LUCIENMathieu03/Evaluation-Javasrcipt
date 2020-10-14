5c11b254ef65a7120b58cc7d0d9c1a2f25fe040a
Lucien Mathieu <lucien.mathieu972@gmail.com>
2020-10-14 21:34:09 +0200

    feat(loadingBar): displays the loading bar

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -3,3 +3,33 @@
 /*
 *Exercice2
 */
+
+
+/**
+* DrawBar
+*@param {number} sum
+*@param {number} nbr
+* @constructor
+*/
+var DrawBar = function DrawBar(sum, nbr) {
+  this.sum = sum;
+  this.nbr = nbr;
+}
+
+/**
+* displayBar
+* @return {DrawBar} this
+*/
+DrawBar.prototype.displayBar = function() {
+  var bar = document.createElement('progress');
+
+  bar.style.width = '100%';
+  bar.setAttribute('max' , this.sum);
+  bar.setAttribute('value' , this.nbr);
+  document.body.appendChild(bar);
+  
+  return this;
+}
+
+var myLoadingBar = new DrawBar(100, 75);
+myLoadingBar.displayBar();
