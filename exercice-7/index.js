9e1a1484839dcee02fb4e6074982e12dc09ae96e
Lucien Mathieu <lucien.mathieu972@gmail.com>
2020-10-14 23:23:30 +0200

    feat(computeNote): create the fuction whitch return the average of a tab of note

--- exercice-7/index.js
+++ exercice-7/index.js
@@ -4,3 +4,24 @@
 *Exercice 7
 */
 
+
+/**
+ * computeNode
+* @constructor
+*/
+var computeNote = function computeNote(noteTab){
+
+	var noteCounter = noteTab.length ;
+	var sum =0;
+
+	for (var i = 0; i < noteTab.length; i++){
+		sum += noteTab[i];
+
+	}
+	return sum/noteCounter;
+
+}
+
+
+var myNote = [10, 13, 13, 12, 15, 12, 11, 16, 14];
+console.log(computeNote(myNote));
