2d0aad2a374da8f7deb843d05baa93a5eff6b035
Lucien Mathieu <lucien.mathieu972@gmail.com>
2020-10-14 19:59:23 +0200

    feat(cellColor): color the cell randomly between 1s & 2s

--- exercice-1/index.js
+++ exercice-1/index.js
@@ -1,21 +1,37 @@
 'use strict'
 
-
 /*
 *Exercice1
 */
 
+
+/**
+ * GridGenerator
+* @constructor
+*/
 var GridGenerator = function GridGenerator(xAxis, yAxis){
-	this.xAxis = xAxis;
-	this.yAxis = yAxis;
-	this.matrix = document.createElement('table');
+  this.xAxis = xAxis;
+  this.yAxis = yAxis;
+  this.matrix = document.createElement('table');
 }
 
+
+/**
+ * run
+* @return {GridGenerator} this
+*/
 GridGenerator.prototype.run = function() {
-	this.renderMatrix();
-	this.renderCells();
+  this.renderMatrix();
+  this.renderCells();
+  setInterval(this.renderCellsColor.bind(this), 2000);
+
+  return this; 
 }
 
+/**
+ * renderMatrix
+* @return {GridGenerator} this
+*/
 GridGenerator.prototype.renderMatrix = function() {
     for(var i = 0; i <= this.yAxis; i++){
         var tr = document.createElement('tr');
@@ -31,16 +47,56 @@ GridGenerator.prototype.renderMatrix = function() {
     }
     
     document.body.appendChild(this.matrix);
+
+    return this;
 }
 
+
+/**
+ * renderCell
+* @return {GridGenerator} this
+*/
 GridGenerator.prototype.renderCells = function() {
   var myCells = document.querySelectorAll('.cells');
 
   for(var i = 0; i < myCells.length; i++) {
-	  myCells[i].style.width = "10px"; 
-  	myCells[i].style.height = "10px";
-  	myCells[i].style.backgroundColor = "red";
+    myCells[i].style.width = "40px"; 
+    myCells[i].style.height = "40px";
   }
+  return this;
+}
+
+
+/**
+ * renderCellColor
+* @return {GridGenerator} this
+*/
+GridGenerator.prototype.renderCellsColor = function() {
+  var myCells = document.querySelectorAll('.cells');
+
+  for(var i=0; i < myCells.length; i++) {
+    var changeInterval = Math.floor(Math.random() * 1000)+1000; // return a number between 1000 & 2000 ([1s , 2s])
+    var randomColor = (Math.floor((Math.random() * 16777215)+1)).toString(16); // return a big int in hexadecimal 
+
+    while (randomColor.length < 6){   // randomColor must have 6 number/letter to represent a color in hexadecimal 
+      randomColor = "0"+ randomColor;
+    }
+    console.log(randomColor);
+
+  setInterval(this.changeCellColor.bind(this), changeInterval, randomColor, myCells[i]);
+  }
+
+}
+
+/**
+ * changeCellColor
+* @param {number} color 
+* @param {number} cell 
+* @return {GridGenerator} this
+*/
+GridGenerator.prototype.changeCellColor = function(color, cell) {
+  cell.style.backgroundColor = '#' + color;
+  return this;
 }
 
 
